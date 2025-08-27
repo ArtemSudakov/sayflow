783e0b563457727af43a7c03c1fa88baeaca664d
Artem <Siteartstudio@gmail.com>
2025-08-27 23:48:43 +0300

    add languages

---
+++ SayFlow/Enums/Screen.swift
@@ -0,0 +1,10 @@
+//
+//  Screen.swift
+//  SayFlow
+//
+//  Created by Asd on 26.08.2025.
+//
+
+enum Screen: String, Codable, Hashable {
+    case home
+}
---
+++ SayFlow/Services/LanguageService.swift
@@ -0,0 +1,7 @@
+//
+//  TranslateService.swift
+//  SayFlow
+//
+//  Created by Asd on 27.08.2025.
+//
+
